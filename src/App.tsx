a524ff3b828c3d0725e8a09ab36034391aba3f5c
Sri Sivasankaran <srisivasankaran@Mac.lan>
2025-09-10 21:02:05 +0530

    refact: Remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Graph from './components/Graph'
 import { ReactFlowProvider } from '@xyflow/react'
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
